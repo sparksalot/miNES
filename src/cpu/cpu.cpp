c0eb58985958b3f9e17e3b177ce9af4f06de6770
Jake Clarke <clarke.jake@gmail.com>
2013-10-13 18:19:21 +0100

    Moved flags into the header file.

--- src/cpu/cpu.cpp
+++ src/cpu/cpu.cpp
@@ -6,16 +6,7 @@
 
 #include "../mem/mem.h"
 // Define
-#define CPU_INCREMENT 4
-
-#define CPU_FLAG_CARRY (1<<0)
-#define CPU_FLAG_ZERO_RESULT (1<<1)
-#define CPU_FLAG_INTERRUPT_DISABLE (<<2)
-#define CPU_FLAG_DECIMAL_MODE (1<<3)
-#define CPU_FLAG_BREAK_COMMAND (1<<4)
-#define CPU_FLAG_EXPANSION (1<<5)
-#define CPU_FLAG_OVERFLOW (1<<6)
-#define CPU_FLAG_NEGATIVE_RESULT (1<<7)
+#define CPU_INCREMENT 1
 
 namespace cpu {
 	Cpu::Cpu() {
--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -10,6 +10,15 @@
 // Define
 #define STACK_BASE 0x0100
 
+#define CPU_FLAG_CARRY (1<<0)
+#define CPU_FLAG_ZERO_RESULT (1<<1)
+#define CPU_FLAG_INTERRUPT_DISABLE (<<2)
+#define CPU_FLAG_DECIMAL_MODE (1<<3)
+#define CPU_FLAG_BREAK_COMMAND (1<<4)
+#define CPU_FLAG_EXPANSION (1<<5)
+#define CPU_FLAG_OVERFLOW (1<<6)
+#define CPU_FLAG_NEGATIVE_RESULT (1<<7)
+
 namespace cpu {
 	class Cpu {
  		public:
