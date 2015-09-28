e081ba8b60ec78a6b7f757b8861ddcb19c33fcc1
Jake Clarke <clarke.jake@gmail.com>
2015-09-28 09:22:03 +0100

    Mem: Added mappings and mirroring

--- src/mem/mem.cpp
+++ src/mem/mem.cpp
@@ -1,6 +1,15 @@
 // Class
 #include "mem.h"
+
 #include <cstring>
+#include <cassert>
+#include <iostream>
+
+#define MEM_MIRROR_END 0x2000
+#define MEM_CART_SPACE_BEGIN 0x4020
+#define MEM_APU_IO_END 0x4020
+#define MEM_PPU_SPACE_END 0x4000
+#define MEM_PPU_SPACE_BEGIN 0x2000
 
 namespace mem {
 	Mem::Mem() {
@@ -11,41 +20,63 @@ namespace mem {
 
 	}
 	void Mem::powerUp() {
-		memset(&this->bank, 0xff, sizeof(Word) * 0x800);
-		bank[0x0008] = 0xF7;
-		bank[0x0009] = 0xEF;
-		bank[0x000a] = 0xDF;
-		bank[0x000f] = 0xBF;
-		bank[0x4017] = 0x00;  // frame irq enabled
-		bank[0x4015] = 0x00;  // silence APU
-		memset(&bank[0x4000], 0x00, sizeof(Word) * 0x10);
+		memset(&this->bank, 0xff, sizeof(bank));
+		store(0xF7, 0x0008);
+		store(0xEF, 0x0009);
+		store(0xDF, 0x000a);
+		store(0xBF, 0x000f);
+		store(0x00, 0x4017);  // frame irq enabled
+		store(0x00, 0x4015);  // silence APU
+
+		// blank apu and ir registers. 
+		//memset(&bank[0x4000], 0x00, sizeof(Word) * 0x10);
 		// changes not shown here also include a, x, y to 0, s to $fd, p to $34
 	}
 
 	void Mem::reset() {
-		bank[0x4015] = 0x00;  // silence APU
+		store(0x00,0x4015); // silence APU
 		// changes not shown here also include S=S-3, IRQ disable flag to true (OR'd with $04)
 	}
 
 	void Mem::store(Word w, uint16_t addr) {
-		this->bank[addr] = w;
+		if(addr < MEM_MIRROR_END) {
+			this->bank[addr % MEM_SIZE] = w;
+		} else if (addr < MEM_PPU_SPACE_END) {
+			// mirrored every 8 bytes.
+			auto ppuAddr = (addr - MEM_PPU_SPACE_BEGIN) % 8;
+			std::cout << "MEM: ppu write to 0x" << std::hex << ppuAddr << std::endl;
+
+		} else if (addr < MEM_APU_IO_END) {
+
+		} else { // 0x4020 space and above is the cart.
+			std::cerr << "MEM: Attempted to write unmapped memory addr: 0x" << std::hex << addr << std::endl;
+			assert(false);
+		}
 	}
 
 	//TODO(nick) guard against (addr+i) >= max address
 	//TODO(nick) consider changing for loop+assignment to a memcpy()
 	void Mem::store(Word * w, size_t size, uint16_t addr) {
 		for(size_t i = 0; i < size; ++i) {
-			bank[addr+i] = w[i];
+			store(w[i], addr+i);
 		}
 	}
 
 	Word Mem::load(uint16_t addr) {
-		return bank[addr];
+		if(addr < MEM_MIRROR_END) {
+			return this->bank[addr % MEM_SIZE];
+		}
+
+		//TODO(jake) write mappings for apu/ppu/cartspace
+
+		std::cerr << "MEM: Attempted to read unmapped memory addr: 0x" << std::hex << addr << std::endl;
+		assert(false);
+		return 0;
 	}
 
 	void Mem::copy(uint16_t addr, Word * w, size_t len) {
 		for(size_t i = 0; i < len; i++) {
-			w[i] = bank[i+addr];
+			w[i] = Mem::load(i+addr);
 		}
 	}
 }
--- src/mem/mem.h
+++ src/mem/mem.h
@@ -5,7 +5,7 @@
 #include <cstdint>
 #include <cstdio>
 // Define
-#define MEM_SIZE 0xFFFF
+#define MEM_SIZE 0x0800
 
 namespace mem {
 	typedef uint8_t Word;
@@ -18,6 +18,7 @@ namespace mem {
 			void store(Word w, uint16_t addr);
 			void store(Word * w, size_t size, uint16_t addr);
 			Word load(uint16_t addr);
+			// copies an area of bank to the destination buffer. Used in DMA.
 			void copy(uint16_t addr, Word * w, size_t len);
 			void reset();
 			void powerUp();
